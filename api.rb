1fb20ce155c0884885fc5b264ecebb7d71acfaad
Peter Hitchcock <eprleads@gmail.com>
2013-03-26 13:06:18 -0700

    Added views

--- api.rb
+++ api.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  erb :index
+end
+
 get '/sushi.json' do
   content_type :json
   return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
