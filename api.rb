8e6af1fa49763751a2da7f218c278f2d8faeb316
Peter Hitchcock <eprleads@gmail.com>
2013-04-01 11:36:28 -0700

    updated api.rb

--- api.rb
+++ api.rb
@@ -7,5 +7,27 @@ end
 
 get '/sushi.json' do
   content_type :json
-  return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+  return {
+  	:sushi => [
+  		"Maguro", 
+  		"Hamachi", 
+  		"Uni", 
+  		"Saba", 
+  		"Ebi", 
+  		"Sake", 
+  		"Tai"]}.to_json
 end
+
+get '/test.json' do
+  content_type :json
+  return {
+  	:test => [
+  		1, 
+  		2, 
+  		3, 
+  		4, 
+  		5, 
+  		6, 
+  		7]}.to_json
+end
+
