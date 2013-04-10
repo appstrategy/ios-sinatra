67a65d42bdbcbb0a276ccd5c62535e053198ee88
Peter Hitchcock <eprleads@gmail.com>
2013-04-10 16:17:32 -0700

    updated gemfile

--- api.rb
+++ api.rb
@@ -1,33 +1,46 @@
 require 'sinatra'
 require 'json'
+require 'mongoid'
 
-get '/' do
-  erb :index
+Mongoid.load!("mongoid.yml")
+ 
+ 
+class Price
+    include Mongoid::Document
+    
+    field :share_id, type: Integer
+    field :date, type: Time
+    field :open, type: Integer
+    field :close, type: Integer
+    field :high, type: Integer
+    field :low, type: Integer
+    field :volume, type: Integer
 end
-
-get '/sushi.json' do
-  content_type :json
-  return {
-  	:sushi => [
-  		"Maguro", 
-  		"Hamachi", 
-  		"Uni", 
-  		"Saba", 
-  		"Ebi", 
-  		"Sake", 
-  		"Tai"]}.to_json
+ 
+class Company
+    include Mongoid::Document
+    
+    field :code, type: String
+    field :sector,    type: String
+    field :share_id, type: Integer
+    field :jse_code, type: Integer
 end
-
-get '/test.json' do
-  content_type :json
-  return {
-  	:test => [
-  		1, 
-  		2, 
-  		3, 
-  		4, 
-  		5, 
-  		6, 
-  		7]}.to_json
+ 
+get '/companies.json' do
+    content_type :json
+    all_companies = Company.all
+    all_companies.to_json
+end
+ 
+ 
+get '/companies/:code/prices.json' do
+    content_type :json
+    company = Company.where(code: params[:code])[0]
+    
+    Price.where('share_id' => company.share_id)
+          .order_by('date', :desc)
+          .to_a
+          .reverse
+          .to_json
 end
 
